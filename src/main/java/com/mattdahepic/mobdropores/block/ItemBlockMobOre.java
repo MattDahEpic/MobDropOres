ad531ade3efc56c83934a19aa80a05eef7b1b443
Matt <mattdahepic@gmail.com>
2015-09-07 09:27:45 -0600

    try to fix itemblock, didnt work

--- src/main/java/com/mattdahepic/mobdropores/block/ItemBlockMobOre.java
+++ src/main/java/com/mattdahepic/mobdropores/block/ItemBlockMobOre.java
@@ -1,12 +1,15 @@
 package com.mattdahepic.mobdropores.block;
 
 import com.mattdahepic.mobdropores.MobDropOres;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 
 public class ItemBlockMobOre extends ItemBlock {
     public ItemBlockMobOre () {
         super(MobDropOres.mob_ore);
+        this.setUnlocalizedName(BlockMobOre.NAME);
+        this.setCreativeTab(CreativeTabs.tabAllSearch);
     }
     @Override
     public String getUnlocalizedName (ItemStack stack) {
