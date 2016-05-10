0d81e6601c03299ebaea1dc97dcff64d5d45813f
MattDahEpic <contact@mattdahepic.com>
2016-05-09 19:01:11 -0600

    update to 1.9 part 3, more work on textures

--- src/main/java/com/mattdahepic/mobdropores/ClientProxy.java
+++ src/main/java/com/mattdahepic/mobdropores/ClientProxy.java
@@ -4,7 +4,6 @@ import com.mattdahepic.mobdropores.block.EnumMob;
 import com.mattdahepic.mobdropores.block.MobUtils;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -14,14 +13,9 @@ public class ClientProxy extends CommonProxy {
     @Override
     public void registerRenderers () {
         Item mob_ore = Item.getItemFromBlock(MobDropOres.mob_ore);
-        String name_prefix = "mob_ore_";
         String modifier_prefix = "mob=";
         for (EnumMob mob : EnumMob.values()) {
-            reg(mob_ore,MobUtils.metaFromMob(mob),MobDropOres.MODID,name_prefix+mob.getName(),modifier_prefix+mob.getName());
+            ModelLoader.setCustomModelResourceLocation(mob_ore,MobUtils.metaFromMob(mob),new ModelResourceLocation("mobdropores:mob_ore",modifier_prefix+mob.getName()));
         }
-    }
-    private void reg (Item item,int meta,String modid,String texture_name,String modifier) {
-        ModelLoader.setCustomModelResourceLocation(item,meta,new ModelResourceLocation(modid+":"+texture_name,"inventory"));
-        ModelLoader.registerItemVariants(item,new ResourceLocation(modid,texture_name));
     }
 }
