213dbc73d318b57cb285752870f9075f04cd5457
MattDahEpic <contact@mattdahepic.com>
2016-07-23 09:25:28 -0600

    fixed block rendering, now blocks wont be placed right...

--- src/main/java/com/mattdahepic/mobdropores/ClientProxy.java
+++ src/main/java/com/mattdahepic/mobdropores/ClientProxy.java
@@ -1,7 +1,10 @@
 package com.mattdahepic.mobdropores;
 
+import com.mattdahepic.mobdropores.block.BlockMobOre;
 import com.mattdahepic.mobdropores.block.EnumMob;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.block.statemap.StateMapperBase;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
@@ -11,10 +14,16 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class ClientProxy extends CommonProxy {
     @Override
     public void registerRenderers () {
-        String modifier_prefix = "mob=";
+        final String modifier_prefix = "mob=";
         for (EnumMob mob : EnumMob.values()) {
             Item block = Item.getItemFromBlock(mob.getBlock());
             ModelLoader.setCustomModelResourceLocation(block,mob.getMeta(),new ModelResourceLocation("mobdropores:mob_ore",modifier_prefix+mob.getName()));
+            ModelLoader.setCustomStateMapper(mob.getBlock(), new StateMapperBase() {
+                @Override
+                protected ModelResourceLocation getModelResourceLocation(IBlockState state) {
+                    return new ModelResourceLocation("mobdropores:mob_ore",modifier_prefix+((EnumMob)state.getValue(BlockMobOre.MOB)).getName());
+                }
+            });
         }
     }
 }
